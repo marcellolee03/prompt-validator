66da8fa24fc4b37b9369841af41344a13f07c438
marcellolee03 <marcellohslee@gmail.com>
2025-11-12 13:08:13 -0300

    fix: setup-autovas.sh bad filepath

--- setup-autovas.sh
+++ setup-autovas.sh
@@ -10,13 +10,13 @@ apt install nmap
 # SETTING UP AUTOVAS ENVIRONMENT
 docker exec -i greenbone-community-edition-gvmd-1 bash -c "mkdir auto_vas"
 
-docker cp src/container_scripts/CreateTarget/create-targets-from-host-list.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/create-targets-from-host-list.gmp.py
-docker cp src/container_scripts/CreateTask/create-tasks-from-csv.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/create-tasks-from-csv.gmp.py
-docker cp src/container_scripts/RunScan/start-scans-from-csv.py greenbone-community-edition-gvmd-1:/auto_vas/start-scans-from-csv.py
-docker cp src/container_scripts/ListReports/list-reports.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/list-reports.gmp.py
-docker cp src/container_scripts/ListReports/export-csv-report.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/export-csv-report.gmp.py
-docker cp src/container_scripts/ListReports/export-xml-report.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/export-xml-report.gmp.py
-docker cp src/container_scripts/ListReports/list-reports.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/list-reports.gmp.py
+docker cp src/AutoVAS/container_scripts/CreateTarget/create-targets-from-host-list.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/create-targets-from-host-list.gmp.py
+docker cp src/AutoVAS/container_scripts/CreateTask/create-tasks-from-csv.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/create-tasks-from-csv.gmp.py
+docker cp src/AutoVAS/container_scripts/RunScan/start-scans-from-csv.py greenbone-community-edition-gvmd-1:/auto_vas/start-scans-from-csv.py
+docker cp src/AutoVAS/container_scripts/ListReports/list-reports.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/list-reports.gmp.py
+docker cp src/AutoVAS/container_scripts/ListReports/export-csv-report.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/export-csv-report.gmp.py
+docker cp src/AutoVAS/container_scripts/ListReports/export-xml-report.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/export-xml-report.gmp.py
+docker cp src/AutoVAS/container_scripts/ListReports/list-reports.gmp.py greenbone-community-edition-gvmd-1:/auto_vas/list-reports.gmp.py
 
 docker exec -i greenbone-community-edition-gvmd-1 bash -c "apt-get update && apt-get install -y python3-venv python3-pip"
 docker exec -i greenbone-community-edition-gvmd-1 bash -c "python3 -m venv path/to/venv && \
